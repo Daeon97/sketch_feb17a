06a5afbe0057b631e99662e9ba99bbb9e1213529
Engels Immanuel <engelmmanuel@gmail.com>
2022-02-18 18:10:40 +0100

    third commit

--- sketch_feb17a.ino
+++ sketch_feb17a.ino
@@ -1,10 +1,41 @@
+#define pinOne 6
+#define pinTwo 7
+#define pinThree 8
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pinOne, OUTPUT);
+  pinMode(pinTwo, OUTPUT);
+  pinMode(pinThree, OUTPUT);
+
+  digitalWrite(pinOne, HIGH);
+  digitalWrite(pinTwo, HIGH);
+  digitalWrite(pinThree, HIGH);
+  delay(2000);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
+  manipPinOne();
+  manipPinTwo();
+  manipPinThree();
+}
+
+void manipPinOne() {
+  digitalWrite(pinOne, HIGH);
+  delay(400);
+  digitalWrite(pinOne, LOW);
   delay(300);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+}
+
+void manipPinTwo() {
+  digitalWrite(pinTwo, HIGH);
+  delay(350);
+  digitalWrite(pinTwo, LOW);
+  delay(400);
+}
+
+void manipPinThree() {
+  digitalWrite(pinThree, HIGH);
+  delay(200);
+  digitalWrite(pinThree, LOW);
+  delay(400);
 }
